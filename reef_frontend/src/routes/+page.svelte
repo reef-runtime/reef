f66b7bfc353f4dd08cce3bb494f4922f041d5792
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-04-30 19:46:09 +0200

    chore: Add SvelteKit frontend skeleton

---
+++ reef_frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = $state(0);
+
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button onclick={increment}>
+  clicks: {count}
+</button>
