6c823f34d60d38a5ab907f74f10e3e487ae67fab
leolurch <40560457+leolurch@users.noreply.github.com>
2024-05-14 17:11:54 +0200

    chore: Init react shadncn/ui project
    ignore false positive linter warning

---
+++ reef_frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import { TooltipProvider } from '@radix-ui/react-tooltip';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+  </React.StrictMode>
+);
--- reef_frontend/vite.config.ts
+++ reef_frontend/vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
