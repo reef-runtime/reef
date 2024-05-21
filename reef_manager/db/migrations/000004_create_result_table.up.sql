67e4b52e7852e6861720bbb93335e60c26d0ee4a
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-05-21 12:20:47 +0200

    feat: Add `job_result` DB migrations

---
+++ reef_manager/db/migrations/000004_create_result_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE
+IF NOT EXISTS
+job_result(
+    -- Foreign key: job ID.
+    job_id          VARCHAR(64) NOT NULL REFERENCES job(id),
+    -- Untyped byte slice in the database, can mean different things.
+    content         BYTEA NOT NULL,
+    -- How the frontend should interpret the content (display as string or hex for instance).
+    content_type    SMALLINT NOT NULL,
+    -- Useful for determining job runtime.
+    created         TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (job_id)
+);
