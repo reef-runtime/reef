c0579099d1f3943facac2095bb8cea7156f92f0c
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-05-28 10:39:51 +0200

    chore: Cleanup node / comms

--- reef_node_native/src/main.rs
+++ reef_node_native/src/main.rs
@@ -13,6 +13,7 @@ use url::Url;
 
 mod handshake;
 mod worker;
+mod comms;
 
 const NODE_REGISTER_PATH: &str = "/api/node/connect";
 
@@ -33,20 +34,6 @@ struct Args {
     tls: bool,
 }
 
-fn log_message(log_kind: u16, worker_index: u16, log_content: &[u8]) -> Message {
-    let mut message = capnp::message::Builder::new_default();
-    let root: reef_protocol::message_capnp::message_from_node::Builder = message.init_root();
-    let mut body = root.init_body().init_job_log();
-
-    body.set_worker_index(worker_index);
-    body.set_log_kind(log_kind);
-    body.set_content(log_content);
-
-    let segments = message.get_segments_for_output();
-    let bin = segments.concat();
-
-    Message::Binary(bin)
-}
 
 #[derive(Debug, Clone)]
 struct Job {
--- reef_node_native/src/worker.rs
+++ reef_node_native/src/worker.rs
@@ -1,8 +1,17 @@
-use std::thread;
+use std::{sync::{mpsc, Arc}, thread};
 
-pub(crate) fn spawn_worker_thread() {
+use crate::comms;
+
+enum WorkerMessage {
+    Log,
+}
+
+type WorkerChannel = Arc<mpsc::Sender<WorkerMessage>>;
+
+pub(crate) fn spawn_worker_thread(worker_index: u16) {
     // TODO: implement this.
-    thread::spawn(|| loop {
+    thread::spawn(move || loop {
         // socket.send(log_message(2, 42, "Hallo".as_bytes())).unwrap();
+        let message = comms::log_message(42, worker_index, "Hallo an alle!".as_bytes());
     });
 }
