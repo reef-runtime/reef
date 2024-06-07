233b51d284e3144d5db751c40db547e8842dfd5c
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-06-08 00:15:41 +0200

    feat: Add ability to run Rust binaries directly with nix

--- flake.nix
+++ flake.nix
@@ -11,22 +11,37 @@
         flake-utils.follows = "flake-utils";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
     flake-utils,
-    ...
+    rust-overlay,
+    crane,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        # ===================
+        # General flake utils
+        # ===================
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inherit (pkgs) lib;
+
+        # =========================
+        # Go toolchain and packages
+        # =========================
+
+        # Custom packaging of teh Capnproto compiler for Go
         capnproto-go = pkgs.buildGoModule {
           # https://github.com/capnproto/go-capnp
           pname = "capnpc-go";
@@ -43,7 +58,6 @@
             "capnpc-go"
           ];
           doCheck = false;
-          # modRoot = "./capnpc-go";
 
           vendorHash = "sha256-3RQBcJKjDacfINr/6jlzjGD7S/T0E223Wl6JPREFTlY=";
 
@@ -51,18 +65,95 @@
             description = "Cap'n Proto library and code generator for Go";
             homepage = "https://github.com/capnproto/go-capnp";
             license = pkgs.lib.licenses.mit;
-            maintainers = [];
           };
         };
+
+        # ===========================
+        # Rust toolchain and packages
+        # ===========================
+
+        rustToolchain = (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = ["rust-src" "rust-std" "rust-analyzer"];
+          targets = ["wasm32-unknown-unknown"];
+        };
+        # Configure crane
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        # cf. https://crane.dev/API.html#libcleancargosource
+
+        # Note: this triggers rebuilds when any files are changed, but none Rust files can't be ignored
+        # because *.capnp files need to be included (otherwise craneLib.cleanCargoSource ./.;)
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        # Build tools
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          git
+
+          rustToolchain
+
+          capnproto
+          capnproto-rust
+          capnproto-go
+        ];
+        # buildInputs = with pkgs; [openssl sqlite];
+        buildInputs = [];
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+
+          strictDeps = true;
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {pname = "reef_dependencies";});
+        # cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        individualCrateArgs =
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            inherit (craneLib.crateNameFromCargoToml {inherit src;}) version;
+          };
+
+        fileSetForCrate = crate:
+          lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions ([
+                # Top level Cargo files
+                ./Cargo.toml
+                ./Cargo.lock
+
+                # All other Cargo.toml need to be included for the top level one to parse
+                ./reef_compiler/Cargo.toml
+                ./reef_interpreter/Cargo.toml
+                ./reef_node_native/Cargo.toml
+                ./reef_protocol/compiler/rust/Cargo.toml
+                ./reef_protocol/node/rust/Cargo.toml
+              ]
+              ++ crate);
+          };
+
+        # Build the top-level crates of the workspace as individual derivations.
+        reef_node_native = craneLib.buildPackage (individualCrateArgs
+          // {
+            pname = "reef_node_native";
+            cargoExtraArgs = "-p reef_node_native";
+            src = fileSetForCrate [./reef_node_native ./reef_interpreter ./reef_protocol];
+          });
+        reef_compiler = craneLib.buildPackage (individualCrateArgs
+          // {
+            # TODO: this is missing all the compiler tools
+            pname = "reef_compiler";
+            cargoExtraArgs = "-p reef_compiler";
+            src = fileSetForCrate [./reef_compiler ./reef_protocol];
+          });
       in {
         devShells.default = pkgs.mkShell {
           name = "Reef Dev";
 
           buildInputs = with pkgs; [
             # Rust toolchain
-            (rustToolchain.override {
-              extensions = ["rust-src" "rust-std" "rust-analyzer"];
-            })
+            rustToolchain
 
             # Golang toolchain
             go_1_21
@@ -112,6 +203,19 @@
         };
 
         formatter = nixpkgs.legacyPackages.${system}.alejandra;
+
+        packages = {
+          inherit reef_node_native reef_compiler;
+        };
+
+        apps = {
+          reef-node-native = flake-utils.lib.mkApp {
+            drv = reef_node_native;
+          };
+          reef-compiler = flake-utils.lib.mkApp {
+            drv = reef_compiler;
+          };
+        };
       }
     );
 }
