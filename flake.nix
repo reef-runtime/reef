373229a8addb666e59c79b5538c91fbf27dd40e0
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-06-07 15:29:45 +0200

    fix: make Rust in flake follow other inputs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = {
