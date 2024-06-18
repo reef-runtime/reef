98358879edc2633104c6315d15797ef9da480fc7
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-06-18 12:16:48 +0200

    chore: Cleanup compiler

--- reef_compiler/src/server.rs
+++ reef_compiler/src/server.rs
@@ -3,7 +3,7 @@ use futures::AsyncReadExt;
 use std::fmt::Display;
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 use std::str::FromStr;
 use std::{fs, io};
@@ -16,13 +16,7 @@ use reef_protocol_compiler::compiler_capnp::{self};
 
 const OUTPUT_FILE: &str = "output.wasm";
 
-struct CompilerManager {
-    build_path: PathBuf,
-    skeleton_path: PathBuf,
-    skip_cleanup: bool,
-}
-
-#[derive(Hash)]
+#[derive(Debug, Clone, Copy, Hash)]
 enum Language {
     C,
     Rust,
@@ -57,14 +51,15 @@ impl Language {
 }
 
 #[derive(Debug)]
-pub enum CError {
+pub enum Error {
     CompilerError(String),
-    SystemError(String),
+    Other(String),
+    Io(io::Error),
 }
 
-impl From<io::Error> for CError {
+impl From<io::Error> for Error {
     fn from(value: io::Error) -> Self {
-        Self::SystemError(value.to_string())
+        Self::Io(value)
     }
 }
 
@@ -72,37 +67,25 @@ impl From<io::Error> for CError {
 // Skeleton directory -> Build directory / <hash of the file>
 //
 
-impl CompilerManager {
-    fn new(main_path: &Path, skeleton_path: &Path, skip_cleanup: bool) -> Self {
-        Self { build_path: main_path.into(), skeleton_path: skeleton_path.into(), skip_cleanup }
-    }
-
-    fn compile(&self, file_buf: &str, language: Language) -> Result<Vec<u8>, CError> {
+impl Compiler {
+    fn compile_inner(&self, file_buf: &str, language: Language) -> Result<Vec<u8>, Error> {
         let mut hasher = DefaultHasher::new();
         file_buf.hash(&mut hasher);
         language.hash(&mut hasher);
 
         let hash = format!("{:x}", hasher.finish());
 
-        let mut wasm_artifact_name = hash.clone();
-        wasm_artifact_name.push_str(".wasm");
-
-        println!("Compiling {wasm_artifact_name}...");
-
         println!("creating build directory...");
 
         let root_build_path = self.build_path.clone();
 
-        match fs::create_dir_all(&root_build_path) {
-            Ok(_) => (),
-            Err(e) => return Err(CError::SystemError(format!("failed to create build directory: {e}"))),
-        };
+        fs::create_dir_all(&root_build_path)?;
 
         let mut skeleton_source_path = self.skeleton_path.clone();
         skeleton_source_path.push(PathBuf::from(&language));
 
         if !skeleton_source_path.exists() {
-            return Err(CError::SystemError(format!("skeleton source path for language {language} does not exist")));
+            return Err(Error::Other(format!("skeleton source path for language {language} does not exist")));
         }
 
         let mut current_compilation_context = root_build_path.clone();
@@ -115,20 +98,14 @@ impl CompilerManager {
         println!("copying {skeleton_source_path:?} to {current_compilation_context:?}...");
 
         if !current_compilation_context.exists() {
-            match copy_dir::copy_dir(&skeleton_source_path, &current_compilation_context) {
-                Ok(_) => (),
-                Err(e) => return Err(CError::SystemError(format!("failed to copy skeleton: {e}"))),
-            }
+            copy_dir::copy_dir(&skeleton_source_path, &current_compilation_context)?;
         }
 
         let mut source_file = current_compilation_context.clone();
         source_file.push(format!("input.{file_ending}", file_ending = language.file_ending()));
 
         println!("writing source file...");
-        match fs::write(source_file, file_buf) {
-            Ok(_) => (),
-            Err(e) => return Err(CError::SystemError(format!("failed to write to source file: {e}"))),
-        };
+        fs::write(source_file, file_buf)?;
 
         let mut cmd = Command::new("make");
         let cmd_args = cmd
@@ -140,18 +117,12 @@ impl CompilerManager {
 
         println!("running command {cmd_args:?}...");
 
-        let output = match cmd_args.output() {
-            Ok(output) => output,
-            Err(e) => {
-                println!("failed to invoke compiler: {e}");
-                return Err(CError::SystemError(format!("failed to invoke compiler: {e}")));
-            }
-        };
+        let output = cmd_args.output()?;
 
         if !output.status.success() {
             let output = String::from_utf8_lossy(&output.stderr);
             println!("failed to invoke compiler: {output}");
-            return Err(CError::CompilerError(output.into_owned()));
+            return Err(Error::CompilerError(output.into_owned()));
         }
 
         let mut output_path = current_compilation_context.clone();
@@ -159,25 +130,21 @@ impl CompilerManager {
 
         println!("reading output file from {output_path:?}...");
 
-        let data = match fs::read(output_path.as_path()) {
-            Ok(data) => data,
-            Err(e) => return Err(CError::SystemError(format!("failed to read output artifact: {e}"))),
-        };
+        let data = fs::read(output_path)?;
 
         // Skip cleanup if required.
-        if self.skip_cleanup {
+        if self.no_cleanup {
             println!("[warning] not performing cleanup...");
             return Ok(data);
         }
 
-        if let Err(err) = fs::remove_dir_all(&current_compilation_context) {
-            return Err(CError::SystemError(format!("failed to cleanup build directory: {err}")));
-        };
+        fs::remove_dir_all(&current_compilation_context)?;
 
         Ok(data)
     }
 }
 
+#[derive(Debug, Clone, Hash)]
 pub(crate) struct Compiler {
     pub(crate) build_path: PathBuf,
     pub(crate) skeleton_path: PathBuf,
@@ -189,23 +156,22 @@ impl compiler::Server for Compiler {
         &mut self,
         params: compiler::CompileParams,
         mut results: compiler::CompileResults,
-    ) -> Promise<(), ::capnp::Error> {
+    ) -> Promise<(), capnp::Error> {
         let program_src = pry!(pry!(pry!(params.get()).get_program_src()).to_str());
         let language = match pry!(pry!(params.get()).get_language()) {
             compiler_capnp::Language::C => Language::C,
             compiler_capnp::Language::Rust => Language::Rust,
         };
 
-        let manager = CompilerManager::new(&self.build_path, &self.skeleton_path, self.no_cleanup);
-        let compiler_res = manager.compile(program_src, language);
+        let compiler_res = self.compile_inner(program_src, language);
 
         match compiler_res {
             Ok(buf) => results.get().init_response().set_file_content(buf.as_slice()),
 
             Err(e) => match e {
-                CError::CompilerError(err) => results.get().init_response().set_compiler_error(err),
-
-                CError::SystemError(err) => results.get().init_response().set_system_error(err.to_string()),
+                Error::CompilerError(err) => results.get().init_response().set_compiler_error(err),
+                Error::Io(err) => results.get().init_response().set_system_error(err.to_string()),
+                Error::Other(err) => results.get().init_response().set_system_error(err),
             },
         }
 
