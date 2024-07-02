4ad8597f206c71bb6f04b0a524037b8685a62498
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-07-02 10:36:02 +0200

    feat: add job results to C Reef std

--- reef_compiler/lang_templates/c/reef.c
+++ reef_compiler/lang_templates/c/reef.c
@@ -13,11 +13,17 @@ void reef_main() {
     size_t len_alloc = (len + 7) & ~0x07;
 
     uint8_t *dataset_mem = malloc(len_alloc);
-
     _reef_dataset_write(dataset_mem);
 
-    run(dataset_mem, len);
+    reef_result_int(0);
 
-    char result[3] = {1, 2, 3};
-    _reef_result(1, (uint8_t *)result, 3);
+    run(dataset_mem, len);
 }
+
+// Result functions
+void reef_result_int(int value) {
+    uint8_t *ptr = (uint8_t *)&value;
+    _reef_result(0, ptr, 4);
+}
+void reef_result_bytes(uint8_t *ptr, size_t len) { _reef_result(1, ptr, len); }
+void reef_result_string(char *ptr, size_t len) { _reef_result(2, (uint8_t *)ptr, len); }
--- reef_compiler/lang_templates/c/reef.h
+++ reef_compiler/lang_templates/c/reef.h
@@ -10,3 +10,8 @@ void reef_sleep(float seconds) __attribute__((__import_module__("reef"), __impor
 
 // User main function definition
 void run(uint8_t *dataset, size_t len);
+
+// Result functions
+void reef_result_int(int value);
+void reef_result_bytes(uint8_t *ptr, size_t len);
+void reef_result_string(char *ptr, size_t len);
