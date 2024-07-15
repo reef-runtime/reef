a3b824eb23382209d38699a762590c50e50a59c9
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2024-07-15 12:58:08 +0200

    fix: frontend build because of iterator keys

--- reef_frontend/src/components/docs-page.tsx
+++ reef_frontend/src/components/docs-page.tsx
@@ -9,11 +9,11 @@ const DocsPage: React.FC<DocsPageProps> = ({ docs }) => {
     <div className="w-full h-full">
       {mapDescription(docs.description)}
       {docs.sections.map((section) => (
-        <section>
+        <section key={section.name}>
           <h3 className="text-xl font-bold mt-4 mb-2">{section.name}</h3>
           {section.description ? mapDescription(section.description) : null}
           {section.entries.map((entry) => (
-            <div>
+            <div key={entry.signature}>
               <p className="font-mono mt-4 mb-1">{entry.signature}</p>
               <div className="ml-2">{mapDescription(entry.description)}</div>
             </div>
@@ -25,4 +25,4 @@ const DocsPage: React.FC<DocsPageProps> = ({ docs }) => {
 };
 export default DocsPage;
 
-const mapDescription = (desc: string[]) => desc.map((p) => <p>{p}</p>);
+const mapDescription = (desc: string[]) => desc.map((p) => <p key={p}>{p}</p>);
