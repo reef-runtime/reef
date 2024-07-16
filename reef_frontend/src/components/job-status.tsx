55dd2626180e5b307d37bcd40c676489d86260ea
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-07-16 23:50:16 +0200

    feat: Add color-class for job util func to web

--- reef_frontend/src/components/job-status.tsx
+++ reef_frontend/src/components/job-status.tsx
@@ -7,11 +7,32 @@ interface JobStatusIconProps {
   job?: IJob | null;
 }
 
+export function colorClassForJob(job?: IJob | null): string {
+  if (!job) {
+    return 'text-gray-300';
+  }
+
+  switch (job.status) {
+    case IJobStatus.StatusQueued:
+      return 'bg-gray-400';
+    case IJobStatus.StatusStarting:
+      return 'bg-sky-500';
+    case IJobStatus.StatusRunning:
+      return 'bg-orange-400';
+    case IJobStatus.StatusDone:
+      return job.result?.success ? 'bg-green-500' : 'bg-red-500';
+    default:
+      throw 'New job status was added without updating this code';
+  }
+}
+
 const JobStatusIcon: FC<JobStatusIconProps> = ({ job }) => {
+  const color = colorClassForJob(job);
+
   if (!job) {
     return (
       <div className="w-5 h-5 flex flex-col justify-center items-center">
-        <BanIcon className="h-4 w-4 text-gray-300" />
+        <BanIcon className={`h-4 w-4 ${color}`} />
       </div>
     );
   }
@@ -21,7 +42,9 @@ const JobStatusIcon: FC<JobStatusIconProps> = ({ job }) => {
       <div className="w-5 h-5 flex flex-col justify-center items-center">
         <span className="relative flex h-3 w-3">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+          <span
+            className={`relative inline-flex rounded-full h-3 w-3 ${color}`}
+          ></span>
         </span>
       </div>
     );
@@ -30,22 +53,16 @@ const JobStatusIcon: FC<JobStatusIconProps> = ({ job }) => {
   if (job?.status === IJobStatus.StatusRunning) {
     return (
       <div className="w-5 h-5 flex flex-col justify-center items-center">
-        <CogIcon className="h-5 w-5 text-orange-400 animate-spin-slow" />
+        <CogIcon
+          className={`h-5 w-5 animate-spin-slow ${color.replaceAll('bg', 'text')}`}
+        />
       </div>
     );
   }
 
   return (
     <div className="w-5 h-5 flex flex-col justify-center items-center">
-      <span
-        className={classNames('w-3 h-3 rounded-full', {
-          'bg-gray-400': job?.status === IJobStatus.StatusQueued,
-          'bg-green-500':
-            job?.status === IJobStatus.StatusDone && job.result?.success,
-          'bg-red-500':
-            job?.status === IJobStatus.StatusDone && !job.result?.success,
-        })}
-      ></span>
+      <span className={`w-3 h-3 rounded-full ${color}`}></span>
     </div>
   );
 };
