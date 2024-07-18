e0f8ba24664127359585e14f3d9ae6657bf4556e
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-07-18 03:00:39 +0200

    feat: Add num worker var. to docker node native

--- flake.nix
+++ flake.nix
@@ -390,7 +390,7 @@
           tag = "latest";
           contents = [reef_node_native pkgs.bash pkgs.cacert];
           config = {
-            Cmd = ["bin/bash" "-c" ''"/bin/reef_node_native" "$REEF_URL"''];
+            Cmd = ["bin/bash" "-c" ''"/bin/reef_node_native" "$REEF_URL" -w "$REEF_WORKERS"''];
           };
         };
       in {
